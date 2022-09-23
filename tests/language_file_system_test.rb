4178597de4b2055ffe1141ac47dc8c624d1d5d2b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-09-23 12:01:30 +0900

    Merge setup and first test

--- tests/language_file_system_test.rb
+++ tests/language_file_system_test.rb
@@ -5,17 +5,14 @@ require_relative './rgss3_mock'
 require_relative '../src/language_file_system'
 
 class TestLanguageFileSystem < LanguageFileSystemTest
-  def setup
+  def test_show_text
     add_file 'DialoguesEnglish.rvtext',
              "# LFS DIALOGUES VERSION 13\n" \
              "<<soldier_greeting>>\n" \
              "\\C[6]Soldier:\\C[0]\n" \
              "Greetings! Don't make any trouble!\n"
-
     init_lfs languages: [:English, :German], default_language: :English
-  end
 
-  def test_show_text
     show_text '\\dialogue[soldier_greeting]'
 
     expected_displayed_text = "\\C[6]Soldier:\\C[0]\n" \
